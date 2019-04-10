258c44f3d42428035da252de6da7a990d2418f10
a-n-n-a-c-g <34349682+a-n-n-a-c-g@users.noreply.github.com>
2019-04-09 18:28:13 -0600

    Update machinelearning.py

--- machinelearning.py
+++ machinelearning.py
@@ -1,36 +1,28 @@
 import passwordmeter
+import os, glob, shutil
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
-CodedPasswords = []
+def main():
+    CodedPasswords = []
+        file = open("Shortpws.txt, "r")
+        for line in file:
+                #print "crap"
+        TSS = []
+        line = fp.readline()
+        cnt = 1
+        while line:
+            print("Line {}: {}".format(cnt, line.strip()))
+            strength, improvements = passwordmeter.test(line)
+            print(int (strength*100))
+            print(cnt)
+            #print("Strength: ".strenth)
+            cnt += 1
+            TSS.append(line)
+            TSS.append(strength)
+            CodedPasswords.append(TSS)
+    print(CodedPasswords)
 
-#fp = open('passwords.txt', 'r')
-with open('Shortpws.txt', 'r') as fp:
-    TSS = []
-    line = fp.readline()
-    cnt = 1
-    while line:
-        print("Line {}: {}".format(cnt, line.strip()))
-        strength, improvements = passwordmeter.test(line)
-        print(int (strength*100))
-        print(cnt)
-        #print("Strength: ".strenth)
-        cnt += 1
-        TSS.append(line)
-        TSS.append(strength)
-        CodedPasswords.append(TSS)
-print(CodedPasswords)
-
-#for password in CodedPasswords:
-       #Strip \n
-
-       #Get strength
-       #strength, improvements = passwordmeter.test(password)
-       
-       #multiply strength by 100 and cast as int
-       #int(strength)*=100
-
-       #add pw and stregnth as a nested list to CodedPasswords
-       #eg [07606374520,10]
-
+if __name__ == "__main__":
+   main()
